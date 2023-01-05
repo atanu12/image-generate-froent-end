c3209bdf4f35e1e9f3036ac2b7dfe74db37e52a1
Atanu Mondal <atanumandal45@gmail.com>
2023-01-06 00:49:24 +0530

    Added the forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 const Home = () => {
   return (
     <>
-    <div className='text-3xl text-center'>Hello Home</div>
-    <button className='rounded-full border-2 border-rose-500 bg-red-900 p-3 text-white'><Link href="/searchimage" >Generate Image</Link></button>
+      <div className="text-3xl text-center">Hello Home</div>
+      <button className="rounded-full border-2 border-rose-500 bg-red-900 p-3 text-white">
+        <Link href="/searchimage">Generate Image</Link>
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
